c103fcc87837be49921a98987223a5fa8f219b83
Daniel Williams <danielpaulwill@gmail.com>
2021-07-26 11:22:01 -0400

    commit tree 16b7446d135492456a8059493f8bb61808758840

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visualFlair = '*') {
+    return function inner1(singlePara = "special") {
+        return `You are ${visualFlair}${singlePara}${visualFlair}!`
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer")
